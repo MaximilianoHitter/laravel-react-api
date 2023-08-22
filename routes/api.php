c21f69383ba8e500e691c82f03e4dfff0d033921
Maximiliano Hitter <mr.vonhumbolt@gmail.com>
2023-08-22 17:56:20 -0300

    tasks

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware'=>'auth:sanctum'], function(){
+    Route::get('/tasks', [TaskController::class, 'index']);
+});
+
+Route::group([], function(){
+    Route::get('/tasksUnsecure', [TaskController::class, 'index']);
 });
 
 
