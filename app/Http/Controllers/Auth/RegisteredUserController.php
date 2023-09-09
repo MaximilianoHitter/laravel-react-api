e030d2d34db262291acd6c9df541ccccf9407d15
Gonzalo Parra <gonzzaparra@gmail.com>
2023-09-09 20:41:07 -0300

    Remove last_name from register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -25,14 +25,13 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'last_name' => ['required', 'string', 'max:255']
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'last_name' => $request->last_name,
+            'last_name' => '',
             'json_socials' => '{}',
             'json_certificates' => '{}',
             'json_speciality' => '{}',
